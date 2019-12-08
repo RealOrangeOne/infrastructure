1b96d96938050061cc3433c4c14918534ef7d847
Jake Howard <git@theorangeone.net>
2019-12-08 12:07:57 +0000

    Add GitHub workflows for linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,8 +6,4 @@ PATH=env/bin:${PATH}
 
 set -x
 
-yamllint -sc yamllint.yml yamllint.yml ansible/
-
-ansible-lint ansible/main.yml -p -c ansible/.ansible-lint
-
-ansible-playbook -i ansible/hosts ansible/main.yml --syntax-check
+yamllint -sc yamllint.yml yamllint.yml .github/
