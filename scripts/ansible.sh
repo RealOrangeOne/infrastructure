58a368335525ee6f6d32cd5c0eaa8c27050b3324
Jake Howard <git@theorangeone.net>
2019-12-08 16:47:28 +0000

    Define haproxy config

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,4 +4,4 @@ set -e
 
 PATH=env/bin:${PATH}
 
-ansible-playbook -i ansible/hosts ansible/main.yml -k -K
+ansible-playbook -i ansible/hosts ansible/main.yml -k -K --vault-password-file ansible/.vault_pass
