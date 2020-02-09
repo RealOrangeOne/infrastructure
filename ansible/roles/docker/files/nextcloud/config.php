4c92e2df3befd26dd298aef9cd200ef3238f7d5d
Jake Howard <git@theorangeone.net>
2020-02-09 15:21:48 +0000

    Add nextcloud config
    Damn this needs cleaning up at some point!

---
+++ ansible/roles/docker/files/nextcloud/config.php
@@ -0,0 +1,48 @@
+<?php
+$CONFIG = array (
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'memcache.distributed' => '\\OC\\Memcache\\Redis',
+  'filelocking.enabled' => true,
+  'redis' =>
+  array (
+    'host' => 'redis',
+    'port' => 6379,
+    'timeout' => 0.0,
+  ),
+  'datadirectory' => '/data',
+  'instanceid' => '{{ nextcloud.instance_id }}',
+  'passwordsalt' => '{{ nextcloud.passwordsalt }}',
+  'secret' => '{{ nextcloud.secret }}',
+  'trusted_domains' =>
+  array (
+    0 => 'intersect.jakehoward.tech',
+    1 => '192.168.1.200',
+    2 => 'onlyoffice.jakehoward.tech',
+  ),
+  'dbtype' => 'mysql',
+  'version' => '18.0.0.10',
+  'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mariadb:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+  'trusted_proxies' =>
+  array (
+    0 => '10.23.0.1/24',
+    1 => '172.0.0.1/8',
+    2 => '192.168.0.0/16',
+  ),
+  'maintenance' => false,
+  'twofactor_enforced' => 'false',
+  'twofactor_enforced_groups' =>
+  array (
+  ),
+  'twofactor_enforced_excluded_groups' =>
+  array (
+  ),
+);
