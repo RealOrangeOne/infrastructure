63aa3a721602d9bbaf4cbdd19f6b410fb5e7685c
Jake Howard <git@theorangeone.net>
2020-02-21 21:51:16 +0000

    Drive part of ansible context from terraform

---
+++ terraform/context.tf
@@ -0,0 +1,8 @@
+resource "local_file" "hosts" {
+    content = yamlencode({
+        hosts: {
+            casey_ip: vultr_server.casey.main_ip
+        }
+    })
+    filename = "${path.module}/../ansible/group_vars/all/hosts.yml"
+}
