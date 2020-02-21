29aee58a2e460a797e89103a5c186041693cc7d4
Jake Howard <git@theorangeone.net>
2020-02-21 22:16:08 +0000

    Format terrsform again

--- terraform/backends.tf
+++ terraform/backends.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "0rng-terraform"
-    key    = "terraform.tfstate"
+    bucket  = "0rng-terraform"
+    key     = "terraform.tfstate"
     encrypt = true
-    region = "eu-west-2"
+    region  = "eu-west-2"
   }
 }
--- terraform/context.tf
+++ terraform/context.tf
@@ -1,8 +1,8 @@
 resource "local_file" "hosts" {
-    content = yamlencode({
-        hosts: {
-            casey_ip: vultr_server.casey.main_ip
-        }
-    })
-    filename = "${path.module}/../ansible/group_vars/all/hosts.yml"
+  content = yamlencode({
+    hosts : {
+      casey_ip : vultr_server.casey.main_ip
+    }
+  })
+  filename = "${path.module}/../ansible/group_vars/all/hosts.yml"
 }
