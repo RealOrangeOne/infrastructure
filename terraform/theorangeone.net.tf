22c0e746c22ebb32d05df4cc05a6878551580d16
Jake Howard <git@theorangeone.net>
2020-03-20 20:57:23 +0000

    Add resources for theorangeone.net forwarding domains

--- terraform/theorangeone.net.tf
+++ terraform/theorangeone.net.tf
@@ -1,3 +1,35 @@
 resource "cloudflare_zone" "theorangeonenet" {
   zone = "theorangeone.net"
 }
+
+resource "cloudflare_record" "theorangeonenet_git" {
+  zone_id = cloudflare_zone.theorangeonenet.id
+  name    = "git"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "theorangeonenet_go" {
+  zone_id = cloudflare_zone.theorangeonenet.id
+  name    = "go"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "theorangeonenet_stats" {
+  zone_id = cloudflare_zone.theorangeonenet.id
+  name    = "stats"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "theorangeonenet_whoami" {
+  zone_id = cloudflare_zone.theorangeonenet.id
+  name    = "whoami"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
