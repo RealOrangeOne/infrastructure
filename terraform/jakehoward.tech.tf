3b6b643d956ee76806951a2cc028f3c0dda29cf9
Jake Howard <git@theorangeone.net>
2020-03-20 21:52:00 +0000

    Add the tonne of records for apps to casey

--- terraform/jakehoward.tech.tf
+++ terraform/jakehoward.tech.tf
@@ -51,3 +51,139 @@ resource "cloudflare_record" "jakehowardtech_dkim_fm3" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "jakehowardtech_wallabag" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "wallabag"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_ttrss" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "tt-rss"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_traefik" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "traefik"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_speed" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "speed"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_quassel" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "quassel"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_portainer" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "portainer"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_pihole" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "pihole"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_onlyoffice" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "onlyoffice"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_netdata" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "netdata"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_media" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "media"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_matrix" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "matrix"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_intersect" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "intersect"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_gotify" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "gotify"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_fah" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "fah"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_duplicati" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "duplicati"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_deluge" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "deluge"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "jakehowardtech_calibre" {
+  zone_id = cloudflare_zone.jakehowardtech.id
+  name    = "calibre"
+  value   = vultr_server.casey.main_ip
+  type    = "A"
+  ttl     = 1
+}
