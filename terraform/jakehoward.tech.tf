dd12b795b5f593dfcdeb872bfc692ff13e70e41d
Jake Howard <git@theorangeone.net>
2020-06-24 18:46:13 +0100

    Remove pihole
    Internal VPN server is working just perfectly instead

--- terraform/jakehoward.tech.tf
+++ terraform/jakehoward.tech.tf
@@ -84,14 +84,6 @@ resource "cloudflare_record" "jakehowardtech_quassel" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "jakehowardtech_pihole" {
-  zone_id = cloudflare_zone.jakehowardtech.id
-  name    = "pihole"
-  value   = vultr_server.casey.main_ip
-  type    = "A"
-  ttl     = 1
-}
-
 resource "cloudflare_record" "jakehowardtech_collabora" {
   zone_id = cloudflare_zone.jakehowardtech.id
   name    = "collabora"
