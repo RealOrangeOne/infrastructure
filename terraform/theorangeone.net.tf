e184c7fba1fae8c9543a57d0f259f6eb0f3490a3
Jake Howard <git@theorangeone.net>
2020-08-04 20:54:53 +0100

    Move website over to walker

--- terraform/theorangeone.net.tf
+++ terraform/theorangeone.net.tf
@@ -79,7 +79,7 @@ resource "cloudflare_record" "theorangeonenet_dkim_fm3" {
 resource "cloudflare_record" "theorangeonenet_www" {
   zone_id = cloudflare_zone.theorangeonenet.id
   name    = "www"
-  value   = vultr_server.grimes.main_ip
+  value   = var.walker_ip
   type    = "A"
   ttl     = 1
 }
@@ -87,7 +87,7 @@ resource "cloudflare_record" "theorangeonenet_www" {
 resource "cloudflare_record" "theorangeonenet_apex" {
   zone_id = cloudflare_zone.theorangeonenet.id
   name    = "@"
-  value   = vultr_server.grimes.main_ip
+  value   = var.walker_ip
   type    = "A"
   ttl     = 1
 }
