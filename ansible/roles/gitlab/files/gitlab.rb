0ce15cb4d81f6e6edcbff3a2903ba5149f20348a
Jake Howard <git@theorangeone.net>
2020-08-29 23:56:14 +0100

    Add gitlab

---
+++ ansible/roles/gitlab/files/gitlab.rb
@@ -0,0 +1,25 @@
+external_url 'https://gitlab.theorangeone.net'
+nginx['redirect_http_to_https'] = false
+nginx['listen_https'] = false
+nginx['listen_port'] = 80
+grafana['enable'] = false
+nginx['status'] = {
+    'enable' => false
+}
+letsencrypt['enable'] = false
+puma['worker_processes'] = 2
+puma['max_threads'] = 6
+gitlab_rails['time_zone'] = 'Europe/London'
+
+alertmanager['enable'] = false
+prometheus_monitoring['enable'] = false
+
+postgresql['enable'] = false
+gitlab_rails['db_adapter'] = 'postgresql'
+gitlab_rails['db_encoding'] = 'unicode'
+gitlab_rails['db_host'] = 'db'
+gitlab_rails['db_password'] = 'gitlab'
+
+redis['enable'] = false
+gitlab_rails['redis_host'] = 'redis'
+gitlab_rails['redis_port'] = 6379
