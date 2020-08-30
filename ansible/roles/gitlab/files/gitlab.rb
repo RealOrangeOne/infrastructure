3b7493ae8f5ee85e255ff2504c9647bbcd72b89d
Jake Howard <git@theorangeone.net>
2020-08-30 21:11:29 +0100

    Set default theme to dark and assign default proxy

--- ansible/roles/gitlab/files/gitlab.rb
+++ ansible/roles/gitlab/files/gitlab.rb
@@ -23,3 +23,6 @@ gitlab_rails['db_password'] = 'gitlab'
 redis['enable'] = false
 gitlab_rails['redis_host'] = 'redis'
 gitlab_rails['redis_port'] = 6379
+
+gitlab_rails['gitlab_default_theme'] = 2
+gitlab_rails['trusted_proxies'] = ['172.17.0.1']
