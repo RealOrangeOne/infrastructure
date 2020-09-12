809a977c636bfe901635539d12339898cd94ee7b
Jake Howard <git@theorangeone.net>
2020-09-12 23:15:08 +0100

    Also update nextcloud config file

--- ansible/roles/intersect-docker/files/nextcloud/config.php
+++ ansible/roles/intersect-docker/files/nextcloud/config.php
@@ -19,7 +19,7 @@ $CONFIG = array (
     0 => 'intersect.jakehoward.tech',
   ),
   'dbtype' => 'pgsql',
-  'version' => '19.0.2.2',
+  'version' => '19.0.3.1',
   'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
   'dbname' => 'nextcloud',
   'dbhost' => 'db',
