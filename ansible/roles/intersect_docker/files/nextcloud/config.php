e9f61070f84848fcffec8950aea96e590e897357
Jake Howard <git@theorangeone.net>
2020-10-07 09:18:32 +0100

    Update nextcloud to version 20
    Using the new LSIO tags for version specific pins! 🎉

--- ansible/roles/intersect_docker/files/nextcloud/config.php
+++ ansible/roles/intersect_docker/files/nextcloud/config.php
@@ -19,7 +19,7 @@ $CONFIG = array (
     0 => 'intersect.jakehoward.tech',
   ),
   'dbtype' => 'mysql',
-  'version' => '19.0.3.1',
+  'version' => '20.0.0.9',
   'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
   'dbname' => 'nextcloud',
   'dbhost' => 'mariadb',
