87120ae0f368934eb7ddfa1e89388bc537c56a4e
Jake Howard <git@theorangeone.net>
2020-12-04 23:07:58 +0000

    Pin terraform provider versions

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,13 +1,16 @@
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "2.11.0"
     }
     vultr = {
-      source = "vultr/vultr"
+      source  = "vultr/vultr"
+      version = "1.4.1"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "3.8.0"
     }
   }
 }
