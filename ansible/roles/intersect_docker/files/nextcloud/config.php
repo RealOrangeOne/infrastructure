af1b7f754c760aa15c5aff7ee376be765d5ff1ce
Jake Howard <git@theorangeone.net>
2020-12-11 17:35:50 +0000

    Update nextcloud to 20.0.3

--- ansible/roles/intersect_docker/files/nextcloud/config.php
+++ ansible/roles/intersect_docker/files/nextcloud/config.php
@@ -19,7 +19,7 @@ $CONFIG = array (
     0 => 'intersect.jakehoward.tech',
   ),
   'dbtype' => 'mysql',
-  'version' => '20.0.1.1',
+  'version' => '20.0.3.2',
   'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
   'dbname' => 'nextcloud',
   'dbhost' => 'mariadb',
