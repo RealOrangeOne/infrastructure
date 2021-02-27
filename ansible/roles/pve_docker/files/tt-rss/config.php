a2fe3ca37ac3b95d3af8b5a4e7ee01bb99c4ceac
Jake Howard <git@theorangeone.net>
2021-02-27 21:29:24 +0000

    Fix TT-RSS config
    It needs to be environment variables now, but there's a bug where it doesn't read docker ones for some reason

--- ansible/roles/pve_docker/files/tt-rss/config.php
+++ ansible/roles/pve_docker/files/tt-rss/config.php
@@ -1,82 +1,11 @@
 <?php
-  // *******************************************
-  // *** Database configuration (important!) ***
-  // *******************************************
+  putenv('TTRSS_DB_USER=tt-rss');
+  putenv('TTRSS_DB_NAME=tt-rss');
+  putenv('TTRSS_DB_PASS=tt-rss');
 
-  define('DB_TYPE', 'pgsql');
-  define('DB_HOST', 'db');
-  define('DB_USER', 'tt-rss');
-  define('DB_NAME', 'tt-rss');
-  define('DB_PASS', 'tt-rss');
-  define('DB_PORT', '5432');
-  define('MYSQL_CHARSET', 'UTF8');
+  putenv('TTRSS_SELF_URL_PATH=https://tt-rss.jakehoward.tech/');
 
-  // ***********************************
-  // *** Basic settings (important!) ***
-  // ***********************************
+  putenv('TTRSS_ENABLE_REGISTRATION=false');
 
-  define('SELF_URL_PATH', 'https://tt-rss.jakehoward.tech/');
-  define('SINGLE_USER_MODE', false);
-  define('SIMPLE_UPDATE_MODE', false);
-
-  // *****************************
-  // *** Files and directories ***
-  // *****************************
-
-  define('PHP_EXECUTABLE', '/usr/bin/php');
-  define('LOCK_DIRECTORY', 'lock');
-  define('CACHE_DIR', 'cache');
-  define('ICONS_DIR', "feed-icons");
-  define('ICONS_URL', "feed-icons");
-
-  // **********************
-  // *** Authentication ***
-  // **********************
-
-  define('AUTH_AUTO_CREATE', true);
-  define('AUTH_AUTO_LOGIN', true);
-
-  // *********************
-  // *** Feed settings ***
-  // *********************
-
-  define('FORCE_ARTICLE_PURGE', 0);
-
-  // ****************************
-  // *** Sphinx search plugin ***
-  // ****************************
-
-  define('SPHINX_SERVER', 'localhost:9312');
-  define('SPHINX_INDEX', 'ttrss, delta');
-
-  // ***********************************
-  // *** Self-registrations by users ***
-  // ***********************************
-
-  define('ENABLE_REGISTRATION', false);
-  define('REG_NOTIFY_ADDRESS', 'user@your.domain.dom');
-  define('REG_MAX_USERS', 10);
-
-  // **********************************
-  // *** Cookies and login sessions ***
-  // **********************************
-
-  define('SESSION_COOKIE_LIFETIME', 864000);
-
-  // *********************************
-  // *** Email and digest settings ***
-  // *********************************
-
-  define('SMTP_FROM_NAME', 'Tiny Tiny RSS');
-  define('SMTP_FROM_ADDRESS', 'noreply@your.domain.dom');
-  define('DIGEST_SUBJECT', '[tt-rss] New headlines for last 24 hours');
-
-  // ************************
-  // *** General settings ***
-  // ************************
-
-  define('CHECK_FOR_UPDATES', false);
-  define('ENABLE_GZIP_OUTPUT', true);
-  define('PLUGINS', 'auth_internal, note');
-  define('LOG_DESTINATION', 'sql');
-  define('CONFIG_VERSION', 26);
+  putenv('TTRSS_CHECK_FOR_UPDATES=false');
+  putenv('TTRSS_ENABLE_GZIP_OUTPUT=true');
