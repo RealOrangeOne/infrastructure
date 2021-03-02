21a2532f8a3ca4e7c7595b93cb4b2d2ba817c1ba
Jake Howard <git@theorangeone.net>
2021-03-02 12:03:13 +0000

    Update nextcloud to 21

--- ansible/roles/pve_docker/files/nextcloud/config.php
+++ ansible/roles/pve_docker/files/nextcloud/config.php
@@ -19,7 +19,7 @@ $CONFIG = array (
     0 => 'intersect.jakehoward.tech',
   ),
   'dbtype' => 'mysql',
-  'version' => '20.0.5.2',
+  'version' => '21.0.0.18',
   'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
   'dbname' => 'nextcloud',
   'dbhost' => 'mariadb',
