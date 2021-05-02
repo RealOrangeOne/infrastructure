b6696e87f28e5598daad8cdbd39a2875fa7939f6
Jake Howard <git@theorangeone.net>
2021-05-02 17:32:27 +0100

    Make 0rng DNS alias primary domain
    CNAME's are good for this

--- terraform/0rng.one.tf
+++ terraform/0rng.one.tf
@@ -5,24 +5,24 @@ resource "cloudflare_zone" "orngone" {
 resource "cloudflare_record" "orngone_git" {
   zone_id = cloudflare_zone.orngone.id
   name    = "git"
-  value   = vultr_instance.casey.main_ip
-  type    = "A"
+  value   = cloudflare_record.theorangeonenet_git.hostname
+  type    = "CNAME"
   ttl     = 1
 }
 
 resource "cloudflare_record" "orngone_who" {
   zone_id = cloudflare_zone.orngone.id
   name    = "who"
-  value   = vultr_instance.casey.main_ip
-  type    = "A"
+  value   = cloudflare_record.theorangeonenet_whoami.hostname
+  type    = "CNAME"
   ttl     = 1
 }
 
 resource "cloudflare_record" "orngone_img" {
   zone_id = cloudflare_zone.orngone.id
   name    = "img"
-  value   = linode_instance.grimes.ip_address
-  type    = "A"
+  value   = cloudflare_record.theorangeonenet_img.hostname
+  type    = "CNAME"
   ttl     = 1
 }
 
