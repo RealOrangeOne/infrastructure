9118938feaacc3e36ef047f867c976654e9a045c
Jake Howard <git@theorangeone.net>
2021-05-29 15:44:01 +0100

    Remove some GitLab constraints
    I don't need things that constrained. The defaults are probably fine, and better tested.

--- ansible/roles/gitlab/files/gitlab.rb
+++ ansible/roles/gitlab/files/gitlab.rb
@@ -15,18 +15,7 @@ gitlab_rails['time_zone'] = '{{ TZ }}'
 
 # https://docs.gitlab.com/omnibus/settings/memory_constrained_envs.html
 puma['worker_processes'] = 2
-sidekiq['max_concurrency'] = 5
-gitaly['ruby_max_rss'] = 200_000_000
-gitaly['cgroups_count'] = 2
-gitaly['cgroups_mountpoint'] = '/sys/fs/cgroup'
-gitaly['cgroups_hierarchy_root'] = 'gitaly'
-gitaly['cgroups_memory_enabled'] = true
-gitaly['cgroups_memory_limit'] = 250000
-gitaly['cgroups_cpu_enabled'] = true
-gitaly['cgroups_cpu_shares'] = 512
-gitaly['env'] = {
-  'GITALY_COMMAND_SPAWN_MAX_PARALLEL' => '2'
-}
+sidekiq['max_concurrency'] = 10
 
 
 gitlab_rails['gitlab_default_theme'] = 2
