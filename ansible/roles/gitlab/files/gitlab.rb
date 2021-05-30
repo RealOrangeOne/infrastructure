64ebaa67d036f8a48ba1c287b8078ca61941cfbe
Jake Howard <git@theorangeone.net>
2021-05-30 21:30:03 +0100

    Setup email for gitlab
    Not super useful, but nice to enable it

--- ansible/roles/gitlab/files/gitlab.rb
+++ ansible/roles/gitlab/files/gitlab.rb
@@ -23,3 +23,14 @@ gitlab_rails['gitlab_default_theme'] = 2
 nginx['real_ip_header'] = 'X-Forwarded-For'
 nginx['real_ip_trusted_addresses'] = ['{{ pve_hosts.pve_docker.ip }}/32']
 gitlab_rails['trusted_proxies'] = ['{{ pve_hosts.pve_docker.ip }}/32']
+
+# SMTP
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "smtp.fastmail.com"
+gitlab_rails['smtp_port'] = 465
+gitlab_rails['smtp_user_name'] = "{{ gitlab_smtp_user }}"
+gitlab_rails['smtp_password'] = "{{ gitlab_smtp_password }}"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_tls'] = true
+gitlab_rails['smtp_openssl_verify_mode'] = 'peer'
+gitlab_rails['gitlab_email_from'] = "{{ gitlab_smtp_user }}"
