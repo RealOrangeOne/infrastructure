a135aae5f34633579b640a8a72119121bdec6297
Jake Howard <git@theorangeone.net>
2021-09-25 16:59:23 +0100

    Provision new VM
    This will be used for monitoring

---
+++ terraform/decker_vps.tf
@@ -0,0 +1,18 @@
+module "decker_firewall" {
+  source = "./vultr_firewall/"
+
+  description = "decker"
+  ports = [
+    "80/tcp",
+    "443/tcp",
+    "7743/tcp"
+  ]
+}
+
+
+resource "vultr_instance" "decker" {
+  plan              = "vc2-1c-1gb"
+  region            = "ams"
+  hostname          = "decker"
+  firewall_group_id = module.decker_firewall.firewall_group.id
+}
