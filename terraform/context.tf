4db474034eb33866a5e3bf10a9004ae3e2387f18
Jake Howard <git@theorangeone.net>
2021-09-27 14:49:56 +0100

    Ignore my VMs from a fail2ban

--- terraform/context.tf
+++ terraform/context.tf
@@ -1,7 +1,10 @@
 resource "local_file" "hosts" {
   content = yamlencode({
     hosts : {
-      casey_ip : vultr_instance.casey.main_ip
+      casey_ip : vultr_instance.casey.main_ip,
+      walker_ip : vultr_instance.walker.main_ip,
+      grimes_ip : vultr_instance.grimes.main_ip,
+      decker_ip : vultr_instance.decker.main_ip,
     }
   })
   filename = "${path.module}/../ansible/group_vars/all/hosts.yml"
