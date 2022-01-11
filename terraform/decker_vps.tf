db68c107d00685505924f1f48e679735ec738357
Jake Howard <git@theorangeone.net>
2022-01-11 19:25:04 +0000

    Decommission decker on Vultr

--- terraform/decker_vps.tf
+++ terraform/decker_vps.tf
@@ -1,24 +1,3 @@
-module "decker_firewall" {
-  source = "./vultr_firewall/"
-
-  description = "decker"
-  ports = [
-    "80/tcp",
-    "443/tcp",
-  ]
-}
-
-
-resource "vultr_instance" "decker" {
-  plan              = "vc2-1c-1gb"
-  region            = "cdg"
-  hostname          = "decker"
-  firewall_group_id = module.decker_firewall.firewall_group.id
-}
-
-
-# Linode
-
 resource "linode_instance" "decker" {
   label      = "decker"
   image      = "linode/arch"
