151c5bc1d99bf0700ed815f88dfc3ad4b57ed7e0
Jake Howard <git@theorangeone.net>
2022-01-30 20:59:33 +0000

    Decommission casey instance in vultr
    The end of an era, my oldest server.

--- terraform/casey_vps.tf
+++ terraform/casey_vps.tf
@@ -1,25 +1,3 @@
-module "casey_firewall" {
-  source = "./vultr_firewall/"
-
-  description = "casey"
-  ports = [
-    "80/tcp",
-    "443/tcp",
-    "51820/udp",
-    "8448/tcp",
-    "6328/udp"
-  ]
-}
-
-resource "vultr_instance" "casey" {
-  plan              = "" # On a plan unsupported by API
-  region            = "lhr"
-  hostname          = "casey"
-  firewall_group_id = module.casey_firewall.firewall_group.id
-}
-
-# Linode
-
 resource "linode_instance" "casey" {
   label      = "casey"
   image      = "linode/arch"
