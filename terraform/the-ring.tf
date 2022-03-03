3ad719a4e7d2226f7a405b17991a1298e673599b
Jake Howard <git@theorangeone.net>
2022-03-03 21:50:24 +0000

    Init a k8s
    May god have mercy on my ~soul~.
    
    The firewall config may not be valid, due to node recycling, but here's hoping!

---
+++ terraform/the-ring.tf
@@ -0,0 +1,28 @@
+resource "linode_lke_cluster" "the-ring" {
+  label       = "the-ring"
+  k8s_version = "1.22"
+  region      = "eu-west"
+
+  pool {
+    type  = "g6-standard-1"
+    count = 1
+  }
+}
+
+resource "linode_firewall" "the-ring" {
+  label = "the-ring"
+  linodes = [
+    for node in linode_lke_cluster.the-ring.pool[0].nodes :
+    node.instance_id
+  ]
+  outbound_policy = "ACCEPT"
+  inbound_policy  = "DROP"
+
+  inbound {
+    label    = "allow-ping"
+    action   = "ACCEPT"
+    protocol = "ICMP"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+}
