5017b3ad78f126add9896a937b4af6e41ea28503
Jake Howard <git@theorangeone.net>
2023-01-29 19:46:47 +0000

    Add mastodon cleanup script

---
+++ ansible/roles/mastodon/files/purge-media.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd /opt/mastodon
+
+docker-compose exec mastodon tootctl media remove --days=7
+docker-compose exec mastodon tootctl statuses remove --days=7
+docker-compose exec mastodon tootctl preview_cards remove --days=7
+docker-compose exec mastodon tootctl cache clear
