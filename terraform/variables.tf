543b8a13568c7292bc70d9b68cb4887e31680236
Jake Howard <git@theorangeone.net>
2023-02-08 20:57:20 +0000

    Mark variables as sensitive

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,23 @@
-variable "vultr_api_key" {}
-variable "cloudflare_api_key" {}
-variable "cloudflare_email" {}
-variable "linode_personal_access_token" {}
-variable "uptimerobot_api_key" {}
-variable "gandi_api_key" {}
+variable "vultr_api_key" {
+    sensitive = true
+}
+
+variable "cloudflare_api_key" {
+    sensitive = true
+}
+
+variable "cloudflare_email" {
+    sensitive = true
+}
+
+variable "linode_personal_access_token" {
+    sensitive = true
+}
+
+variable "uptimerobot_api_key" {
+    sensitive = true
+}
+
+variable "gandi_api_key" {
+    sensitive = true
+}
