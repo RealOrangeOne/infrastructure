2af9f8529d45b93325eded719d2cb6823455ad6f
Jake Howard <git@theorangeone.net>
2023-06-15 15:16:19 +0100

    Fix new ansible-lint errors
    Quite a few changes here, hopefully they work!

--- terraform/context.tf
+++ terraform/context.tf
@@ -1,10 +1,10 @@
 resource "local_file" "hosts" {
   content = yamlencode({
-    hosts : {
+    vps_hosts : {
       casey_ip : linode_instance.casey.ip_address,
       walker_ip : vultr_instance.walker.main_ip,
       grimes_ip : vultr_instance.grimes.main_ip,
     }
   })
-  filename = "${path.module}/../ansible/group_vars/all/hosts.yml"
+  filename = "${path.module}/../ansible/group_vars/all/vps-hosts.yml"
 }
