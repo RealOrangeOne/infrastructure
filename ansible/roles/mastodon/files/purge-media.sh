56b846f38c417e8eda740404632aeabd544763ef
Jake Howard <git@theorangeone.net>
2023-07-02 16:42:30 +0100

    Prune mastodon accounts and orphan media

--- ansible/roles/mastodon/files/purge-media.sh
+++ ansible/roles/mastodon/files/purge-media.sh
@@ -4,7 +4,9 @@ set -ex
 
 cd /opt/mastodon
 
+docker-compose exec mastodon tootctl accounts prune
 docker-compose exec mastodon tootctl media remove --days=7
 docker-compose exec mastodon tootctl statuses remove --days=7
 docker-compose exec mastodon tootctl preview_cards remove --days=7
+docker-compose exec mastodon tootctl media remove-orphans
 docker-compose exec mastodon tootctl cache clear
