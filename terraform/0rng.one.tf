37b8c48a7744deea7d366ba4a7526e08508b5498
Jake Howard <git@theorangeone.net>
2023-10-02 09:37:05 +0100

    Remove legacy short domains
    I never used them, and the certificate renewal didn't work anyway.

--- terraform/0rng.one.tf
+++ terraform/0rng.one.tf
@@ -2,36 +2,6 @@ resource "gandi_livedns_domain" "orngone" {
   name = "0rng.one"
 }
 
-resource "gandi_livedns_record" "orngone_git" {
-  zone = gandi_livedns_domain.orngone.id
-  name = "git"
-  type = "CNAME"
-  ttl  = 3600
-  values = [
-    "${cloudflare_record.theorangeonenet_git.hostname}."
-  ]
-}
-
-resource "gandi_livedns_record" "orngone_who" {
-  zone = gandi_livedns_domain.orngone.id
-  name = "who"
-  type = "CNAME"
-  ttl  = 3600
-  values = [
-    "${cloudflare_record.theorangeonenet_whoami.hostname}."
-  ]
-}
-
-resource "gandi_livedns_record" "orngone_img" {
-  zone = gandi_livedns_domain.orngone.id
-  name = "img"
-  type = "CNAME"
-  ttl  = 3600
-  values = [
-    "${cloudflare_record.theorangeonenet_img.hostname}."
-  ]
-}
-
 resource "gandi_livedns_record" "orngone_apex" {
   zone = gandi_livedns_domain.orngone.id
   name = "@"
