1555803d25a489d88b547dee7a0a4d27e8bd573e
Jake Howard <git@theorangeone.net>
2024-01-06 21:28:57 +0000

    Move terraform state file
    This lets me use the bucket for other states too

--- terraform/backends.tf
+++ terraform/backends.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "0rng-terraform"
-    key     = "terraform.tfstate"
+    key     = "infrastructure/terraform.tfstate"
     encrypt = true
     region  = "eu-west-2"
   }
