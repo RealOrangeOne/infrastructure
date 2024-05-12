bbfe0e802cad70c18c60df5dea26b71a44ba72c3
Jake Howard <git@theorangeone.net>
2024-05-12 16:14:29 +0100

    Remove Walker VPS in Vultr

--- terraform/walker_vps.tf
+++ terraform/walker_vps.tf
@@ -1,33 +1,3 @@
-module "walker_firewall" {
-  source = "./vultr_firewall/"
-
-  description = "walker"
-  ports = [
-    "80/tcp",
-    "443/tcp",
-    "41641/udp"
-  ]
-}
-
-resource "vultr_instance" "walker" {
-  plan              = "vhf-1c-2gb"
-  region            = "lhr"
-  hostname          = "walker"
-  firewall_group_id = module.walker_firewall.firewall_group.id
-}
-
-resource "vultr_reverse_ipv4" "walker_reverse_ipv4" {
-  instance_id = vultr_instance.walker.id
-  ip          = vultr_instance.walker.main_ip
-  reverse     = cloudflare_record.sys_domain_walker.hostname
-}
-
-resource "vultr_reverse_ipv6" "walker_reverse_ipv6" {
-  instance_id = vultr_instance.walker.id
-  ip          = vultr_instance.walker.v6_main_ip
-  reverse     = cloudflare_record.sys_domain_walker.hostname
-}
-
 resource "hcloud_server" "walker" {
   name               = "walker"
   image              = "debian-12" # Not true - that was just the initial install.
