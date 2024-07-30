905f327ec51711f83cbaffd445f0c34a5fe93073
Jake Howard <git@theorangeone.net>
2024-07-30 19:51:39 +0100

    Update Nextcloud to 29.0.4

--- ansible/roles/pve_docker/files/nextcloud/config.php
+++ ansible/roles/pve_docker/files/nextcloud/config.php
@@ -19,7 +19,7 @@ $CONFIG = array (
     0 => 'intersect.jakehoward.tech',
   ),
   'dbtype' => 'mysql',
-  'version' => '29.0.3.4',
+  'version' => '29.0.4.1',
   'overwrite.cli.url' => 'https://intersect.jakehoward.tech',
   'dbname' => 'nextcloud',
   'dbhost' => 'mariadb',
