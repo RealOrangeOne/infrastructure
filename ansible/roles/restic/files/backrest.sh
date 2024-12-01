df5b8f4c90bdaaf3a3ce950ed5c0e348c43adbc3
Jake Howard <git@theorangeone.net>
2024-12-01 19:17:54 +0000

    Use s3 backend for restic backups
    It's apparently more stable

--- ansible/roles/restic/files/backrest.sh
+++ ansible/roles/restic/files/backrest.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-export B2_ACCOUNT_ID="{{ restic_b2_account_id }}"
-export B2_ACCOUNT_KEY="{{ restic_b2_account_key }}"
+export AWS_ACCESS_KEY_ID="{{ restic_b2_account_id }}"
+export AWS_SECRET_ACCESS_KEY="{{ restic_b2_account_key }}"
 export RESTIC_PASSWORD="{{ restic_key }}"
-export RESTIC_REPOSITORY="b2:{{ restic_b2_bucket }}"
+export RESTIC_REPOSITORY="s3:{{ restic_b2_endpoint }}/{{ restic_b2_bucket }}"
 export GOGC=20  # HACK: Work around for restic's high memory usage https://github.com/restic/restic/issues/1988
 
 set -x
