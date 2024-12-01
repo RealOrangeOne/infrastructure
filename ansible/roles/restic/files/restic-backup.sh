55df3e8fa340bb539b4c717bf64b29776e302cec
Jake Howard <git@theorangeone.net>
2024-12-01 20:58:34 +0000

    Separate restic repositories
    Smaller VMs no longer need to load the full index

--- ansible/roles/restic/files/restic-backup.sh
+++ ansible/roles/restic/files/restic-backup.sh
@@ -3,3 +3,5 @@
 set -e
 
 exec $HOME/backrest.sh --verbose backup --files-from=$HOME/restic-include.txt --exclude-file=$HOME/restic-excludes.txt
+
+exec $HOME/backrest.sh forget --prune --keep-daily 30 --keep-monthly 3 --group-by host
