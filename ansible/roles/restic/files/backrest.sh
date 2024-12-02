c66e9f368f4938dec53911e7c864df55d1786778
Jake Howard <git@theorangeone.net>
2024-12-02 22:28:57 +0000

    Use separate access keys per restic repository
    PVE TBC

--- ansible/roles/restic/files/backrest.sh
+++ ansible/roles/restic/files/backrest.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-export AWS_ACCESS_KEY_ID="{{ restic_b2_account_id }}"
-export AWS_SECRET_ACCESS_KEY="{{ restic_b2_account_key }}"
-export RESTIC_PASSWORD="{{ restic_key }}"
+export AWS_ACCESS_KEY_ID="{{ vault_restic_b2_application_key_id }}"
+export AWS_SECRET_ACCESS_KEY="{{ vault_restic_b2_application_key }}"
+export RESTIC_PASSWORD="{{ vault_restic_key }}"
 export RESTIC_REPOSITORY="s3:{{ restic_b2_endpoint }}/{{ restic_b2_bucket }}/{{ restic_dir }}"
 export GOGC=20  # HACK: Work around for restic's high memory usage https://github.com/restic/restic/issues/1988
 
