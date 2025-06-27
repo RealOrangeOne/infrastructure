2528d8630ae73cad82c37a5b0859ab71a7372ffe
Jake Howard <git@theorangeone.net>
2025-06-27 22:47:20 +0100

    Tweak mastodon clean commands

--- ansible/roles/mastodon/files/purge-media.sh
+++ ansible/roles/mastodon/files/purge-media.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# Based on https://ricard.dev/improving-mastodons-disk-usage/
+
 set -ex
 
 cd /opt/mastodon
 
 docker-compose exec mastodon tootctl accounts prune
-docker-compose exec mastodon tootctl media remove --days=7
-docker-compose exec mastodon tootctl statuses remove --days=7
-docker-compose exec mastodon tootctl preview_cards remove --days=7
+docker-compose exec mastodon tootctl media remove
+docker-compose exec mastodon tootctl media remove --prune-profiles
+docker-compose exec mastodon tootctl preview_cards remove --days=21
 docker-compose exec mastodon tootctl media remove-orphans
-docker-compose exec mastodon tootctl cache clear
+docker-compose exec mastodon tootctl statuses remove --days=180
