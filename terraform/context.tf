f6649c85592808a5ef73819660b7c73d43882f19
Jake Howard <git@theorangeone.net>
2025-06-27 22:12:45 +0100

    Add grimes to context vars

--- terraform/context.tf
+++ terraform/context.tf
@@ -5,6 +5,7 @@ resource "local_file" "hosts" {
       private_ipv6_marker : local.private_ipv6_marker,
       private_ipv6_range : local.private_ipv6_range,
       walker_ip : hcloud_server.walker.ipv4_address,
+      grimes_ip : hcloud_server.grimes.ipv4_address,
     }
   })
   filename = "${path.module}/../ansible/group_vars/all/vps-hosts.yml"
