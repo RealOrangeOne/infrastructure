689d95c9c9d5a190ccff6e2f143ebbe7e19d7c89
Jake Howard <git@theorangeone.net>
2026-01-14 21:56:09 +0000

    Manage desec terraform token in terraform

--- terraform/desec.tf
+++ terraform/desec.tf
@@ -1,9 +1,16 @@
-# Must be created manually and imported, otherwise there's no way to access the token
+# NOTE: Tokens must be created manually and imported, otherwise there's no way to access the token
 resource "desec_token" "traefik" {
   name               = "traefik"
   perm_create_domain = false
   perm_delete_domain = false
   perm_manage_tokens = false
+}
+
+resource "desec_token" "terraform" {
+  name               = "Terraform"
+  perm_create_domain = false
+  perm_delete_domain = false
+  perm_manage_tokens = true
 }
 
 resource "desec_token_policy" "traefik_default" {
