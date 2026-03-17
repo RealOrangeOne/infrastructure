78931fda7703c606bad87a6e307b4bfd5162c549
Jake Howard <git@theorangeone.net>
2026-03-17 22:10:07 +0000

    Fix restic prunes

--- ansible/roles/restic/files/restic-backup.sh
+++ ansible/roles/restic/files/restic-backup.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-exec $HOME/backrest.sh --verbose backup --files-from=$HOME/restic-include.txt --exclude-file=$HOME/restic-excludes.txt
+$HOME/backrest.sh --verbose backup --files-from=$HOME/restic-include.txt --exclude-file=$HOME/restic-excludes.txt
 
-exec $HOME/backrest.sh forget --prune --keep-daily 30 --keep-monthly 3 --group-by host
+$HOME/backrest.sh forget --prune --keep-daily 30 --keep-monthly 3 --group-by host
