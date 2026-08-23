f3b64824a782dd295907517de42fedbdbf7881f4
Jake Howard <git@theorangeone.net>
2026-08-23 15:14:56 +0100

    Reduce mastodon retention

--- ansible/roles/mastodon/files/purge-media.sh
+++ ansible/roles/mastodon/files/purge-media.sh
@@ -7,8 +7,8 @@ set -ex
 cd /opt/mastodon
 
 docker compose exec mastodon tootctl accounts prune
-docker compose exec mastodon tootctl media remove
+docker compose exec mastodon tootctl media remove --keep-interacted
 docker compose exec mastodon tootctl media remove --prune-profiles
-docker compose exec mastodon tootctl preview_cards remove --days=180
+docker compose exec mastodon tootctl preview_cards remove --days=90
 docker compose exec mastodon tootctl media remove-orphans
-docker compose exec mastodon tootctl statuses remove --days=180
+docker compose exec mastodon tootctl statuses remove --days=90
